5a38ed52107b35d4fdc16d8abbc879d417077ff8
sohanemon <m.sohanemon@gmail.com>
2024-12-11 14:33:05 +0600

    fix(package.json): bumped version from 4.0.27 to 4.0.28

---
+++ src/types/guards.ts
@@ -0,0 +1,29 @@
+export type Primitive =
+  | string
+  | number
+  | bigint
+  | boolean
+  | symbol
+  | null
+  | undefined;
+export type Falsy = false | '' | 0 | null | undefined;
+export type Nullish = null | undefined;
+
+export const isFalsy = (val: unknown): val is Falsy => !val;
+export const isNullish = (val: unknown): val is Nullish => val == null;
+export const isPrimitive = (val: unknown): val is Primitive => {
+  if (val === null || val === undefined) {
+    return true;
+  }
+  switch (typeof val) {
+    case 'string':
+    case 'number':
+    case 'bigint':
+    case 'boolean':
+    case 'symbol': {
+      return true;
+    }
+    default:
+      return false;
+  }
+};
---
+++ src/types/utilities.ts
@@ -0,0 +1,59 @@
+export type Keys<T extends object> = keyof T;
+export type Values<T extends object> = T[keyof T];
+
+export type DeepPartial<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T extends object
+      ? { [K in keyof T]?: DeepPartial<T[K]> }
+      : T;
+
+export type SelectivePartial<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
+
+export type DeepRequired<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+    ? Array<DeepRequired<U>>
+    : T extends object
+      ? { [K in keyof T]-?: DeepRequired<T[K]> }
+      : T;
+
+export type DeepReadonly<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+    ? ReadonlyArray<DeepReadonly<U>>
+    : T extends object
+      ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+      : T;
+
+export type Mutable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
+
+export type KeysOfType<T, U> = {
+  [K in keyof T]: T[K] extends U ? K : never;
+}[keyof T];
+
+export type OmitByType<T, U> = {
+  [K in keyof T as T[K] extends U ? never : K]: T[K];
+};
+
+export type RequiredKeys<T, K extends keyof T> = Omit<T, K> &
+  Required<Pick<T, K>>;
+
+export type Diff<T, U> = Omit<T, keyof U> & Omit<U, keyof T>;
+
+export type Intersection<T extends object, U extends object> = Pick<
+  T,
+  Extract<keyof T, keyof U> & Extract<keyof U, keyof T>
+>;
+
+export type Merge<
+  T extends object,
+  U extends object,
+  I = Diff<T, U> & Intersection<U, T> & Diff<U, T>,
+> = Pick<I, keyof I>;
+
+export type Substract<T extends object, U extends object> = Omit<T, keyof U>;
