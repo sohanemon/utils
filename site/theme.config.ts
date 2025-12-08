6f846c93cf61f149106608c16622456af2da78e0
sohanemon <sohanemon@outlook.com>
2025-12-08 14:35:12 +0600

    feat(site): added new files and dependencies

---
+++ site/theme.config.ts
@@ -0,0 +1,13 @@
+export default {
+  logo: 'Utils',
+  project: {
+    link: 'https://github.com/sohanemon/utils',
+  },
+  docsRepositoryBase: 'https://github.com/sohanemon/utils/blob/main/site/pages',
+  footer: {
+    text: '© 2025 Utils',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+};
--- theme.config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  sidebar: {
-    defaultMenuCollapseLevel: 1,
-  },
-};
